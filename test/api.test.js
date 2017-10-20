d62a9e930e14bc1dbb742dda5cb4521098a63db0
Jon <jon@Jon.local>
2017-10-20 16:36:26 -0700

    first tests against server

--- test/api.test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const Lab = require('lab'),
-	{ describe, it, expect } = exports.lab = Lab.script()
-
-it('returns true when 1 + 1 equals 2', (done) => {
-
-	expect(1 + 1).to.equal(2)
-	done()
-})
